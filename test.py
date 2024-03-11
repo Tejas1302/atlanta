8f88cc0175c382bdcc36cf3c56c7902914f97c76
Tejas1302 <84171086+Tejas1302@users.noreply.github.com>
2024-03-11 12:16:45 +0000

    ok

--- test.py
+++ test.py
@@ -6,6 +6,8 @@ import snowflake.connector
 from urllib.error import URLError
 import streamlit as st
 from snowflake.snowpark.context import get_active_session
+from pathlib import Path
+import base64
 
 # if 'layout_preference' not in st.session_state:
 #     st.session_state['layout_preference'] = 'wide'
@@ -17,12 +19,20 @@ def create_session():
 session = create_session()
 
 # Write directly to the app
+# def img_to_bytes(img_path):
+#     img_bytes = Path(img_path).read_bytes()
+#     encoded = base64.b64encode(img_bytes).decode()
+#     return encoded
+
+st.image("logo.png")
+#st.markdown('''[<img src='data:image/png;base64,{}' class='img-fluid' width=200 height=100>]'''.format(img_to_bytes("logo.png")), unsafe_allow_html=True)
+st.header("Team : Saama Thunder's") 
 st.title("Welcome to Streamlit in Snowflake")
-st.header("Saama Thunder's") 
-status = st.radio("Select One of the Stage: ", ('Internal Satge', 'External Stage(S3)','Named Stage'))
+
+status = st.radio("Select One of the Stage: ", ('Internal Stage', 'External Stage(S3)','Named Stage'))
 
 # conditional statement to print 
-if (status == 'Internal Satge'):
+if (status == 'Internal Stage'):
     st.success("You have selected Internal Stage")
 elif (status=='External Stage(S3)'):
     st.success("You have selected External Stage(S3)")
@@ -30,3 +40,17 @@ elif(status=='Named Stage'):
     st.success("You have selected Named Stage")
 else:
     st.info("You haven't selected anything")
+
+if status == "Internal Stage":
+    st.subheader("There are no files in Internal Stage")
+
+if status == "Named Stage":
+    st.subheader("There are no files in Named Stage")
+
+if status == "External Stage(S3)":
+    st.subheader("External Stage Files:")
+
+df = session.sql("LIST @STORE_DB.ATLAS.AWS_S3_STG")
+
+
+
