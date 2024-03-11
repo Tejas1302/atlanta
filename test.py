9e530e3b8c1c44e744e52693ffd540e733ca0918
Tejas1302 <84171086+Tejas1302@users.noreply.github.com>
2024-03-11 05:38:24 +0000

    ok

--- test.py
+++ test.py
@@ -1,1 +1,16 @@
+# import python packages
+from snowflake.snowpark import session
+import pandas as pd
+import requests
+import snowflake.connector
+from urllib.error import URLError
+import streamlit as st 
+from snowflake.snowpark.context import get_active_session
 
+def create_session():
+    return session.builder.configs(st.secrets['snowflake']).create()
+
+session = create_session()
+
+#write directly to the app
+st.title("Welcome to Streamlit in Snwoflake")
