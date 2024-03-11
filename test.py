ddf7f535ad10b2c774af78306ea7d7b552b69974
Tejas1302 <84171086+Tejas1302@users.noreply.github.com>
2024-03-11 14:28:36 +0000

    ok

--- test.py
+++ test.py
@@ -29,7 +29,7 @@ st.image("logo.png")
 st.header("Team : Saama Thunder's") 
 st.title("Welcome to Streamlit in Snowflake")
 
-status = st.radio("Select One of the Stage: ", ('Internal Stage', 'External Stage(S3)','Named Stage'))
+status = st.radio("Select One of the Stage :", ('Internal Stage', 'External Stage(S3)','Named Stage'))
 
 # conditional statement to print 
 if (status == 'Internal Stage'):
@@ -48,9 +48,98 @@ if status == "Named Stage":
     st.subheader("There are no files in Named Stage")
 
 if status == "External Stage(S3)":
-    st.subheader("External Stage Files:")
+    st.subheader("External Stage Files :")
 
-df = session.sql("LIST @STORE_DB.ATLAS.AWS_S3_STG")
+    df = session.sql("LIST @STORE_DB.ATLAS.AWS_S3_STG").collect()
+
+    st.dataframe(data=df, use_container_width=True)
+
+    sql_filename = "Select distinct METADATA$FILENAME from @STORE_DB.ATLAS.AWS_S3_STG"
+
+    df_filename =  session.sql(sql_filename).collect()
+
+    files = st.selectbox("Please select any one file from the below list : ",(df_filename))
+
+    if 'clicked' not in st.session_state:
+        st.session_state.clicked = False
+
+    def click_button():
+        st.session_state.clicked = True
+
+    st.button('PREVIEW', on_click=click_button, type = 'primary')
+
+    # Preview code
+    if st.session_state.clicked:
+        # The message and nested widget will remain on the page
+        st.write("You have selected",files)
+        # extract the table name from the file name
+        table_name = files.replace(' ','')[:-4].upper()
+
+        sql_truncate = f"TRUNCATE TABLE STORE_DB.ATLAS.{table_name};"
+        session.sql(sql_truncate).collect()
+        sql_copy = f"COPY INTO STORE_DB.ATLAS.{table_name} from @STORE_DB.ATLAS.AWS_S3_STG/{files} file_format = F1"
+        session.sql(sql_copy).collect()
+        sql_select = f"select * from STORE_DB.ATLAS.{table_name} limit 10"
+        dataframe_select = st.dataframe(session.sql(sql_select).collect())
+
+        #column names
+        sql_columns = f"select column_name from information_schema.columns where table_name = '{table_name}' and table_schema = 'ATLAS';"
+        df_sql_columns = session.sql(sql_columns).collect()
+        df_columns = pd.DataFrame(df_sql_columns)
+
+        # Multiselect
+        st.subheader('Data Profiling')
+        st.caption('Please select any one column')
+
+        #Checkbox
+        #Function to create checkbox
+        def checkbox_container(data):
+            #select_column_box = st.text_input('Please select any column')
+            cols = st.columns(5)
+            if cols[0].button('Select All', type = 'primary'):
+                for i in data['COLUMN_NAME']:
+                    st.session_state['dynamic_checkbox_' + i] = True
+                st.experimental_rerun()
+            if cols[1].button('UnSelect All',type='primary'):
+                for i in data['COLUMN_NAME']:
+                    st.session_state['dynamic_checkbox_' + i] = False
+                st.experimental_rerun()
+            for i in data['COLUMN_NAME']:
+                	st.checkbox(i, key='dynamic_checkbox_' + i)
+
+        #Function to print selected columns    
+        def get_selected_checkboxes():
+            return [i.replace('dynamic_checkbox_','') for i in st.session_state.keys() if i.startswith('dynamic_checkbox_') and st.session_state[i]]
+
+        checkbox_container(df_columns)
+        new_data = st.text_input('You selected',get_selected_checkboxes())
+
+        selected_column = get_selected_checkboxes()   
+
+        for values in selected_column:
+            #sql query to preview the data for selected columns
+            # to show the distinct records
+            select_sql = f"select distinct {values} from {table_name}"
+            collect_select_sql = session.sql(select_sql).collect()
+            df_select_sql = pd.DataFrame(collect_select_sql)
+            st.write(df_select_sql)
+
+        
+        col1, col2 = st.columns(2)
+
+        with col1:
+            st.header("Distinct Record")
+            select_sql = f"select distinct {values} from {table_name}"
+            collect_select_sql = session.sql(select_sql).collect()
+            df_select_sql = pd.DataFrame(collect_select_sql)
+            st.write(df_select_sql)
+
+        with col2:
+            st.header("Counts")
+            count_sql = f"select {values},count(*) as Total from {table_name} group by {values}"
+            collect_count_sql = session.sql(count_sql).collect()
+            df_count_sql = pd.DataFrame(collect_count_sql)
+            st.write(df_count_sql)
 
 
 
